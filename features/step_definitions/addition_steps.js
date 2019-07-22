b5dbd1eb771d246696a223bd99fc2c2df8138b8b
ruralkan <ruralkan@gmail.com>
2019-07-22 16:15:57 -0500

    Implementing the second step - use the calculator

--- features/step_definitions/addition_steps.js
+++ features/step_definitions/addition_steps.js
@@ -5,13 +5,12 @@ const Calculator = require('../../lib/calculator');
 let calculator;
 
 Given('the numbers {int} and {int}', function (int, int2, callback) {
-    calculator = new Calculator();
+    calculator = new Calculator(x, y);
     callback();
   });
 
-When('they are added together', function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+When('they are added together', function () {
+    calculator.add();
   });
 
 
