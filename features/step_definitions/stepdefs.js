f4c1b2bf0fe5c19e7b874a8cdb16dfa98e2a3d3e
ruralkan <ruralkan@gmail.com>
2019-07-22 14:54:04 -0500

    Adding test to features

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,17 +1,18 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 
+function isItFriday(today) {
+    return 'Nope';
+  }
+
 Given('today is Sunday', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    this.today = 'Sunday';
   });
   
 When('I ask whether it\'s Friday yet', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    this.actualAnswer = isItFriday(this.today);
   });
 
-Then('I should be told {string}', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('I should be told {string}', function (expectedAnswer) {
+    assert.equal(this.actualAnswer, expectedAnswer);
   });
