d6ae2d50f094e81ea7317861dc1b7c993f96bc57
ruralkan <ruralkan@gmail.com>
2019-07-22 15:03:30 -0500

    Using variables and examples

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,25 +2,21 @@ const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 
 function isItFriday(today) {
-    if (today === "Friday") {
-        return "TGIF"; 
-      } else {
-        return "Nope";
-      }
+  if (today === "Friday") {
+    return "TGIF";
+  } else {
+    return "Nope";
   }
+}
 
-Given('today is Sunday', function () {
-    this.today = 'Sunday';
-  });
-  
+Given('today is {string}', function (givenDay) {
+  this.today = givenDay;
+});
+
 When('I ask whether it\'s Friday yet', function () {
-    this.actualAnswer = isItFriday(this.today);
-  });
+  this.actualAnswer = isItFriday(this.today);
+});
 
 Then('I should be told {string}', function (expectedAnswer) {
-    assert.equal(this.actualAnswer, expectedAnswer);
-  });
-
-  Given('today is Friday', function () {
-    this.today = 'Friday';
-  });
+  assert.equal(this.actualAnswer, expectedAnswer);
+});
