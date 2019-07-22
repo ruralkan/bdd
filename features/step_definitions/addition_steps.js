879055424b3fb0f5352facb7a41f971bb3fe40b9
ruralkan <ruralkan@gmail.com>
2019-07-22 16:19:34 -0500

    Implementing the third step - verify the calculation

--- features/step_definitions/addition_steps.js
+++ features/step_definitions/addition_steps.js
@@ -1,21 +1,17 @@
-const assert = require('assert');
-const { Given, When, Then } = require('cucumber');
+const assert = require('assert')
+const {Before, Given, When, Then} = require('cucumber');
 const Calculator = require('../../lib/calculator');
 
 let calculator;
 
-Given('the numbers {int} and {int}', function (int, int2, callback) {
+Given('the numbers {int} and {int}', function (x, y) {
     calculator = new Calculator(x, y);
-    callback();
-  });
+});
 
 When('they are added together', function () {
     calculator.add();
-  });
+});
 
-
-Then('should the result be {int}', function (int, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
-  });
-
+Then('should the result be {int}', function (expected) {
+    assert.equal(calculator.getResult(), expected)
+});
